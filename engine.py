8b15246abc2d513f642c9efa7989d91329cd1805
ashirwad-maker <ashirwadsinha@gmail.com>
2024-04-30 12:39:56 +0530

    Implemented Engine and Nueural Network

---
+++ engine.py
@@ -0,0 +1,84 @@
+class Value:
+    def __init__(self, value, _children = (), op=''):
+        self.data = value
+        self.grad = 0.0
+        self._prev = set(_children)
+        self._backward = lambda : None 
+    
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data,(self,other),'+')
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad+= 1.0 * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other),'*')
+        
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+        return out
+
+
+    def __pow__(self, other):
+        out = Value(self.data** other,(self,),'**')
+        def _backward():
+            self.grad+= other*self.data**(other-1) * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def relu(self):
+        out = Value(0 if self.data<0 else self.data, (self,),"relu")
+        def _backward():
+            self.grad += (1 if self.data>0 else 0) *out.grad
+
+        self._backward = _backward
+        return out
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(node):
+            if node not in visited:
+                visited.add(node)
+                for child in node._prev:
+                    build_topo(child)
+                topo.append(node)
+
+        build_topo(self)
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
+
+    def __neg__(self):
+        return self*-1
+    
+    def __radd__(self,other):
+        return self + other
+    
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return other + (-self)
+    
+    def __truediv__(self,other):
+        return self * (other**-1)
+    
+    def __rtruedeiv__(self, other):
+        return other * self**-1
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __repr__(self):
+        return f"Value(Data ={self.data} and Grad = {self.grad})"
---
+++ neural_network.py
@@ -0,0 +1,59 @@
+import math
+from engine import Value
+
+class Module:
+    def zero_grad(self):
+        for p in self.parameters:
+            p.grad=0
+
+    def parameter(self):
+        return []
+    
+
+class Neuron(Module):
+    def __init__(self, nin, nonLinear = True):
+        self.w = [Value(math.random(-1,1)) for _ in nin]
+        self.b = Value(0)
+        self.nonLinear = nonLinear 
+
+    def __call__(self, x):
+        act = sum((wi*xi for wi,xi in zip(self, x)),self.b)
+        return act.relu() if self.nonLinear else act
+    
+    def parameters(self):
+        return self.w + [self.b]
+    
+    def __repr__(self):
+        return f"{"ReLu" if self.nonLinear else "Linear"} Neuron({len(self.w)})"  
+    
+
+class Layer(Module):
+    def __init__(self,nin,nout,**kwargs):
+        self.neuron = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self,x):
+        out = [n(x) for n in self.neuron]
+        return out[0] if len(out)==1 else out
+
+    def parameters(self):
+        return [p for n in self.neuron for p in n.parameters()]
+    
+    def __repr__(self):
+        return f"Layer of [{','.join(str(n) for n in self.neuron)}]"
+    
+
+class MLP(Module):
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layer = [Layer(sz[i],sz[i+1],nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+
+    def __call__(self,x):
+        for layer in self.layer:
+            x = layer(x)
+        return x
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+    
+    def __repr__(self):
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
